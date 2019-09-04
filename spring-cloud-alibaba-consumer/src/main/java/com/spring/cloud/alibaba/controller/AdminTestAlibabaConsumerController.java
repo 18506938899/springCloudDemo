20bde35879c9ef49a7b49c7b38e7b773b568f6db
shaokai.zhang <kb248521>
2019-09-04 11:28:30 +0800

    springCloud alibaba版学习1

--- spring-cloud-alibaba-consumer/src/main/java/com/spring/cloud/alibaba/controller/AdminTestAlibabaConsumerController.java
+++ spring-cloud-alibaba-consumer/src/main/java/com/spring/cloud/alibaba/controller/AdminTestAlibabaConsumerController.java
@@ -27,7 +27,8 @@ public class AdminTestAlibabaConsumerController {
 
     @RequestMapping(value = "/testAlibabaConsumerByFeign.shtml")
     public String testAlibabaConsumerByFeign(){
-        String resutl = testAlibabaConsumerService.testAlibabaConsumerByFeign();
+        String resutl = testAlibabaConsumerService
+                .testAlibabaConsumerByFeign();
         return resutl;
     }
 }
