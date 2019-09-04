a5fb4ad2710286b1bf149509138e5572e7fa227c
shaokai.zhang <kb248521>
2019-09-04 11:21:28 +0800

    springCloud alibaba版学习

--- spring-cloud-alibaba-provider/src/main/java/com/spring/cloud/alibaba/controller/AdminTestAlibabaProviderController.java
+++ spring-cloud-alibaba-provider/src/main/java/com/spring/cloud/alibaba/controller/AdminTestAlibabaProviderController.java
@@ -3,6 +3,13 @@ package com.spring.cloud.alibaba.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * @author shaokai.zhang
  * @date 2019-08-29
@@ -14,5 +21,40 @@ public class AdminTestAlibabaProviderController {
     public String testAlibabaProvider(){
         System.out.println("testAlibabaProvider");
         return "testAlibabaProvider";
+    }
+
+    public static void main(String[] args){
+        URL url = null;
+        URLConnection urlconn = null;
+        BufferedReader br = null;
+        PrintWriter pw = null;
+        String regex = "http://[\\w+\\.?/?]+\\.[A-Za-z]+";
+        Pattern p = Pattern.compile(regex);
+        try {
+            url = new URL("https://blog.csdn.net/u012027337/article/details/51741372");
+            urlconn = url.openConnection();
+            pw = new PrintWriter(new FileWriter("d:/url.txt"), true);//这里我们把收集到的链接存储在了E盘底下的一个叫做url的txt文件中
+            br = new BufferedReader(new InputStreamReader(
+                    urlconn.getInputStream()));
+            String buf = null;
+            while ((buf = br.readLine()) != null) {
+                Matcher buf_m = p.matcher(buf);
+                while (buf_m.find()) {
+                    pw.println(buf_m.group());
+                }
+            }
+            System.out.println("获取成功！");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                br.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            pw.close();
+        }
     }
 }
